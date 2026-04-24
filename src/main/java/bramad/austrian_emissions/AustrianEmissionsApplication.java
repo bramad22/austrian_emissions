67ce94691d88df1be5a299f0f91455a4deb0a512
bramad <bramad22@htl-kaindorf.at>
2026-04-24 11:18:12 +0200

    Initial commit

---
+++ src/main/java/bramad/austrian_emissions/AustrianEmissionsApplication.java
@@ -0,0 +1,13 @@
+package bramad.austrian_emissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustrianEmissionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AustrianEmissionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bramad/austrian_emissions/AustrianEmissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package bramad.austrian_emissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AustrianEmissionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
