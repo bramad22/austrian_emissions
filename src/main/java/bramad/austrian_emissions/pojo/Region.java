7248d889f5cf6830f3cdb5755bab82e3d1cd4a5f
bramad <bramad22@htl-kaindorf.at>
2026-05-04 08:53:12 +0200

    implemented Region Pojo

--- src/main/java/bramad/austrian_emissions/pojo/Region.java
+++ src/main/java/bramad/austrian_emissions/pojo/Region.java
@@ -1,4 +1,18 @@
 package bramad.austrian_emissions.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Region {
+    @Id
+    private String id;
+
+    private String name;
 }
