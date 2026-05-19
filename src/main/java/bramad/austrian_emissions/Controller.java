a50e2d887d5b6523522ccc7c7d8f3533d7735a79
bramad <bramad22@htl-kaindorf.at>
2026-05-19 14:21:14 +0200

    Applied changes to database (consistent name)

--- src/main/java/bramad/austrian_emissions/Controller.java
+++ src/main/java/bramad/austrian_emissions/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/emissions")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class Controller {
 
